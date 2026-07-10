be849cecb8b57d340ad03222f4078b9c16239e22
take-beekeeping <takedacoco@icloud.com>
2026-07-11 07:08:53 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="beenote-v7";
+const CACHE_NAME="beenote-v8";
 const FILES=["./","./index.html","./manifest.json","./beenote-icon-180.png","./beenote-icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
